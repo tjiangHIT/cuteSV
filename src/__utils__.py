b4dd9017c117f14919d857eb8ccafac38a5952d6
tjiang <tjiang@hit.edu.cn>
2018-09-17 15:23:57 +0800

    SV length needs to be recalculated

--- src/__utils__.py
+++ src/__utils__.py
@@ -1,5 +1,6 @@
 import cigar
 import gc, sys
+import time
 # INS_flag = {1:'I'}
 # DEL_flag = {2:'D'}
 
@@ -33,8 +34,8 @@ def detect_flag(Flag):
 
 def search_indel_str(deal_cigar, pos_start, SV_size, Chr_name, RLength):
 	seq = list(cigar.Cigar(deal_cigar).items())
-	Ins_list = list()
-	Del_list = list()
+	# Ins_list = list()
+	# Del_list = list()
 	shift_ins = 0
 	shift_del = 0
 	for element in seq:
@@ -42,14 +43,16 @@ def search_indel_str(deal_cigar, pos_start, SV_size, Chr_name, RLength):
 			shift_ins += element[0]
 		if element[1] == 'I' and element[0] > SV_size:
 			shift_ins += 1
-			Ins_list.append([Chr_name, pos_start + shift_ins, element[0]])
+			# Ins_list.append([Chr_name, pos_start + shift_ins, element[0]])
+			store_signal([[Chr_name, pos_start + shift_ins, element[0]]], "INS")
 
 		if element[1] == 'M':
 			shift_del += element[0]
 		if element[1] == 'D' and element[0] < SV_size:
 			shift_del += element[0]
 		if element[1] == 'D' and element[0] >= SV_size:
-			Del_list.append([Chr_name, pos_start + shift_del, element[0]])
+			# Del_list.append([Chr_name, pos_start + shift_del, element[0]])
+			store_signal([[Chr_name, pos_start + shift_del, element[0]]], "DEL")
 			shift_del += element[0]
 
 	if seq[0][1] == 'S':
@@ -62,11 +65,11 @@ def search_indel_str(deal_cigar, pos_start, SV_size, Chr_name, RLength):
 		softclip_right = 0
 	clip_list = [softclip_left, RLength - softclip_right, pos_start, pos_start + shift_ins, Chr_name]
 
-	return Ins_list, Del_list, clip_list
+	return clip_list
 
 def search_indel_list(deal_cigar, pos_start, SV_size, Chr_name, RLength):
-	Ins_list = list()
-	Del_list = list()
+	# Ins_list = list()
+	# Del_list = list()
 	shift_ins = 0
 	shift_del = 0
 	# _shift_read_ = 0
@@ -77,14 +80,16 @@ def search_indel_list(deal_cigar, pos_start, SV_size, Chr_name, RLength):
 		# 	_shift_read_ += element[1]
 		if element[0] == 1 and element[1] > SV_size:
 			shift_ins += 1
-			Ins_list.append([Chr_name, pos_start + shift_ins, element[1]])
+			# Ins_list.append([Chr_name, pos_start + shift_ins, element[1]])
+			store_signal([[Chr_name, pos_start + shift_ins, element[1]]], "INS")
 			
 		if element[0] == 0:
 			shift_del += element[1]
 		if element[0] == 2 and element[1] < SV_size:
 			shift_del += element[1]
 		if element[0] == 2 and element[1] >= SV_size:
-			Del_list.append([Chr_name, pos_start + shift_del, element[1]])
+			# Del_list.append([Chr_name, pos_start + shift_del, element[1]])
+			store_signal([[Chr_name, pos_start + shift_del, element[1]]], "DEL")
 			shift_del += element[1]
 
 	if deal_cigar[0][0] == 4:
@@ -97,7 +102,7 @@ def search_indel_list(deal_cigar, pos_start, SV_size, Chr_name, RLength):
 		softclip_right = 0
 
 	clip_list = [softclip_left, RLength - softclip_right, pos_start + 1, pos_start + shift_ins + 1, Chr_name]
-	return Ins_list, Del_list, clip_list
+	return clip_list
 
 def store_info(pos_list, svtype):
 	for ele in pos_list:
@@ -130,11 +135,11 @@ def analysis_split_read(split_read, SV_size, RLength):
 	SP_list = list()
 	for read in split_read:
 		if isinstance(read[3], str):
-			Ins_list, Del_list, sp_list = search_indel_str(read[3], read[1], SV_size, read[0], RLength)
+			sp_list = search_indel_str(read[3], read[1], SV_size, read[0], RLength)
 		else:
-			Ins_list, Del_list, sp_list = search_indel_list(read[3], read[1], SV_size, read[0], RLength)
-		store_signal(Del_list, "DEL")
-		store_signal(Ins_list, "INS")
+			sp_list = search_indel_list(read[3], read[1], SV_size, read[0], RLength)
+		# store_signal(Del_list, "DEL")
+		# store_signal(Ins_list, "INS")
 
 		sp_list += read[2]
 		SP_list.append(sp_list)
@@ -148,34 +153,66 @@ def analysis_split_read(split_read, SV_size, RLength):
 	#  	print i
 
 	DUP_flag = [0]*len(SP_list)
-	INVDUP_flag = [0]*len(SP_list)
+	# INVDUP_flag = [0]*len(SP_list)
 	# INS_flag = [0]*len(SP_list)
 
-	for a in SP_list:
-		for b in SP_list:
-			if a[4] == b[4]:
-				# dup & INV & TRA & INS
-				if b[3] - a[2] >= SV_size and SP_list.index(a) > SP_list.index(b):
-					DUP_flag[SP_list.index(a)] = 1
-					DUP_flag[SP_list.index(b)] = 1
-				if a[0] + b[3] - a[2] - b[1] >= SV_size and b[3] <= a[2] and SP_list.index(a) == SP_list.index(b) + 1:
-					store_signal([[a[4], (a[2]+b[3])/2, a[0]+b[3]-a[2]-b[1]]], "INS")
-			else:
-				# tra
-				if SP_list.index(a) > SP_list.index(b):
-					if b[4] < a[4]:
-						store_signal([[b[4], b[3], a[4], a[2]]], "TRA")
-					else:
-						store_signal([[a[4], a[2], b[4], b[3]]], "TRA")
+	# for a in SP_list:
+	# 	for b in SP_list:
+	# 		if a[4] == b[4]:
+	# 			# dup & INV & TRA & INS & DEL
+	# 			if b[3] - a[2] >= SV_size and SP_list.index(a) > SP_list.index(b):
+	# 				DUP_flag[SP_list.index(a)] = 1
+	# 				DUP_flag[SP_list.index(b)] = 1
+	# 			if a[0] + b[3] - a[2] - b[1] >= SV_size and b[3] <= a[2] and SP_list.index(a) == SP_list.index(b) + 1:
+	# 				store_signal([[a[4], (a[2]+b[3])/2, a[0]+b[3]-a[2]-b[1]]], "INS")
+	# 			if a[2] - a[0] + b[1] - b[3] >= SV_size and b[3] <= a[2] and SP_list.index(a) == SP_list.index(b) + 1:
+	# 				store_signal([[a[4], b[3], a[2]-a[0]+b[1]-b[3]]], "DEL")
+	# 		else:
+	# 			# tra
+	# 			if SP_list.index(a) > SP_list.index(b):
+	# 				if b[4] < a[4]:
+	# 					store_signal([[b[4], b[3], a[4], a[2]]], "TRA")
+	# 				else:
+	# 					store_signal([[a[4], a[2], b[4], b[3]]], "TRA")
 
-	for k in xrange(len(SP_list)):
-		if DUP_flag[k] == 1:
-			if k == 0:
-				store_info([[SP_list[k][4], SP_list[k][3]]], "DUP_r")
-			elif k == len(SP_list)-1:
-				store_info([[SP_list[k][4], SP_list[k][2]]], "l_DUP")
+	for a in xrange(len(SP_list[:-1])):
+		ele_1 = SP_list[a]
+		ele_2 = SP_list[a+1]
+		if ele_1[4] == ele_2[4]:
+			# dup & ins & del 
+			if ele_1[3] - ele_2[2] >= SV_size:
+				# DUP_flag[a] = 1
+				# DUP_flag[a+1] = 1
+				if a == 0:
+					store_info([[ele_1[4], ele_1[3]]], "DUP_r")
+
+				if a+1 == len(SP_list)-1:
+					store_info([[ele_2[4], ele_2[2]]], "l_DUP")
+				else:
+					store_signal([[ele_2[4], ele_2[2], ele_2[3]]], "DUP")
+
+			if ele_1[3] <= ele_2[2]:
+				if ele_2[0] + ele_1[3] - ele_2[2] - ele_1[1] >= SV_size:
+					store_signal([[ele_2[4], (ele_2[2]+ele_1[3])/2, ele_2[0]+ele_1[3]-ele_2[2]-ele_1[1]]], "INS")
+				if ele_2[2] - ele_2[0] + ele_1[1] - ele_1[3] >= SV_size:
+					store_signal([[ele_2[4], ele_1[3], ele_2[2]-ele_2[0]+ele_1[1]-ele_1[3]]], "DEL")
+		else:
+			# tra
+			if ele_1[4] < ele_2[4]:
+				store_signal([[ele_1[4], ele_1[3], ele_2[4], ele_2[2]]], "TRA")
 			else:
-				store_signal([[SP_list[k][4], SP_list[k][2], SP_list[k][3]]], "DUP")
+				store_signal([[ele_2[4], ele_2[2], ele_1[4], ele_1[3]]], "TRA")
+
+
+
+	# for k in xrange(len(SP_list)):
+	# 	if DUP_flag[k] == 1:
+	# 		if k == 0:
+	# 			store_info([[SP_list[k][4], SP_list[k][3]]], "DUP_r")
+	# 		elif k == len(SP_list)-1:
+	# 			store_info([[SP_list[k][4], SP_list[k][2]]], "l_DUP")
+	# 		else:
+	# 			store_signal([[SP_list[k][4], SP_list[k][2], SP_list[k][3]]], "DUP")
 
 	# +TRA++TRA++TRA++TRA++TRA++TRA++TRA++TRA+
 
@@ -249,17 +286,20 @@ def merge_pos_indel(pos_list, chr, evidence_read, SV_size, svtype):
 		size.sort()
 		breakpoint = sum(start)/len(start)
 		SV_len = sum(size[int(0.25*len(size)):int(0.75*len(size))])/len(size[int(0.25*len(size)):int(0.75*len(size))])
+		# concider the stability of data
+		# need to repair the buf!!!!!!!!
 
 		if SV_len >= SV_size and size[int(0.75*len(size))] - size[int(0.25*len(size))] < int(0.5*SV_len):
 			# result.append([chr, breakpoint, SV_len, len(pos_list)])
 			if chr not in semi_result:
 				semi_result[chr] = list()
-			semi_result[chr].append([breakpoint, SV_len, len(pos_list), svtype])
+			# semi_result[chr].append([breakpoint, SV_len, len(pos_list), svtype])
+			semi_result[chr].append("%d\t%d\t%d\t%s\n"%(breakpoint, SV_len, len(pos_list), svtype))
 
 	# return result
 
 def intergrate_indel(chr, evidence_read, SV_size, low_bandary, svtype, max_distance):
-	_cluster_ = list()
+	# _cluster_ = list()
 	temp = list()
 	temp.append(candidate[svtype][chr][0])
 	for pos in candidate[svtype][chr][1:]:
@@ -334,7 +374,7 @@ def merge_pos_dup(pos_list, chr, evidence_read, SV_size, ll, lr, svtype):
 	breakpoint = sum(start+re_l)/len(start+re_l)
 	size = sum(end+re_r)/len(end+re_r) - breakpoint
 
-	print breakpoint, size, re_r, re_l, start, end
+	# print breakpoint, size, re_r, re_l, start, end
 
 	# result = list()
 	if len(pos_list+re_l+re_r) >= evidence_read:
@@ -343,7 +383,8 @@ def merge_pos_dup(pos_list, chr, evidence_read, SV_size, ll, lr, svtype):
 			# result.append([chr, breakpoint, size, len(pos_list+re_l+re_r)])
 			if chr not in semi_result:
 				semi_result[chr] = list()
-			semi_result[chr].append([breakpoint, size, len(pos_list+re_l+re_r), svtype])
+			# semi_result[chr].append([breakpoint, size, len(pos_list+re_l+re_r), svtype])
+			semi_result[chr].append("%d\t%d\t%d\t%s\n"%(breakpoint, size, len(pos_list+re_l+re_r), svtype))
 
 def intergrate_dup(chr, evidence_read, SV_size, low_bandary, ll, lr, svtype, max_distance):
 	temp = list()
@@ -405,7 +446,8 @@ def merge_pos_tra(pos_list, chr, evidence_read, SV_size, svtype, low_bandary):
 
 			if chr not in semi_result:
 				semi_result[chr] = list()
-			semi_result[chr].append([breakpoint_1, max_chr, breakpoint_2, svtype])
+			# semi_result[chr].append([breakpoint_1, max_chr, breakpoint_2, svtype])
+			semi_result[chr].append("%d\t%s\t%s\t%s\n"%(breakpoint_1, max_chr, breakpoint_2, svtype))
 
 def intergrate_tra(chr, evidence_read, SV_size, low_bandary, svtype, max_distance):
 	temp = list()
@@ -421,38 +463,64 @@ def intergrate_tra(chr, evidence_read, SV_size, low_bandary, svtype, max_distanc
 	if temp[-1][0] - temp[0][0] <= max_distance:
 		merge_pos_tra(temp, chr, evidence_read, SV_size, svtype, low_bandary)
 
-def show_temp_result(evidence_read, SV_size, low_bandary, max_distance):
+def show_temp_result(evidence_read, SV_size, low_bandary, max_distance, out_path):
+	starttime = time.time()
 	for chr in candidate["DEL"]:
 		candidate["DEL"][chr] = sorted(candidate["DEL"][chr], key = lambda x:x[0])
 		# for ele in candidate["DEL"][chr]:
 		# 	print chr, ele
 		intergrate_indel(chr, evidence_read, SV_size, low_bandary, "DEL", max_distance)
+		candidate["DEL"][chr] = []
+		gc.collect()
+	print("[INFO]: Parse deletions used %0.2f seconds."%(time.time() - starttime))
 
-
+	starttime = time.time()
 	for chr in candidate["INS"]:
 		candidate["INS"][chr] = sorted(candidate["INS"][chr], key = lambda x:x[0])
 		# for i in candidate["INS"][chr]:
 		# 	print chr, i
 		intergrate_indel(chr, evidence_read, SV_size, low_bandary, "INS", max_distance)
+		candidate["INS"][chr] = []
+		gc.collect()
+	print("[INFO]: Parse insertions used %0.2f seconds."%(time.time() - starttime))
 
+	starttime = time.time()
 	for chr in candidate["DUP"]:
 		candidate["DUP"][chr] = sorted(candidate["DUP"][chr], key = lambda x:x[:])
-		for i in candidate["DUP"][chr]:
-			print chr, i
+		# for i in candidate["DUP"][chr]:
+		# 	print chr, i
 		intergrate_dup(chr, evidence_read, SV_size, low_bandary, "l_DUP", "DUP_r", "DUP", max_distance)
+		candidate["DUP"][chr] = []
+		candidate["l_DUP"][chr] = []
+		candidate["DUP_r"][chr] = []
+		gc.collect()
+	print("[INFO]: Parse duplications used %0.2f seconds."%(time.time() - starttime))
 	
+	starttime = time.time()
 	for chr in candidate["INV"]:
 		candidate["INV"][chr] = sorted(candidate["INV"][chr], key = lambda x:x[:])
 		intergrate_dup(chr, evidence_read, SV_size, low_bandary, "l_INV", "INV_r", "INV", max_distance)
+		candidate["INV"][chr] = []
+		candidate["l_INV"][chr] = []
+		candidate["INV_r"][chr] = []
+		gc.collect()
+	print("[INFO]: Parse inversions used %0.2f seconds."%(time.time() - starttime))
 
+	starttime = time.time()
 	for chr in candidate["TRA"]:
 		candidate["TRA"][chr] = sorted(candidate["TRA"][chr], key = lambda x:x[:])
 		# for i in candidate["TRA"][chr]:
 		# 	print chr, i
 		intergrate_tra(chr, evidence_read, SV_size, low_bandary, "TRA", max_distance)
+		candidate["TRA"][chr] = []
+		gc.collect()
+	print("[INFO]: Parse translocations used %0.2f seconds."%(time.time() - starttime))
 
+	file = open(out_path, 'w')
 	for chr in semi_result:
 		semi_result[chr] = sorted(semi_result[chr], key = lambda x:x[0])
 		for i in semi_result[chr]:
-			print i
+			# print i
+			file.write("%s\t%s"%(chr, i))
+	file.close()
 	# print candidate['DEL']
--- src/contrl.py
+++ src/contrl.py
@@ -5,11 +5,13 @@ import pysam
 import cigar
 from multiprocessing import Pool
 from __utils__ import *
+import time
 
 
 
 
-def main_ctrl(sam_path):
+def main_ctrl(sam_path, out_path):
+	starttime = time.time()
 	samfile = pysam.AlignmentFile(sam_path)
 	process_list = list()
 	for i in samfile.get_index_statistics():
@@ -17,14 +19,17 @@ def main_ctrl(sam_path):
 		# #chr #read
 	process_list = sorted(process_list, key = lambda x:-x[1])
 	Chr_name = process_list[0][0]
+	print("[INFO]: %d reads on %s."%(process_list[0][1], process_list[0][0]))
 	# Chr_length = samfile.get_reference_length(Chr_name)
 	# print Chr_name, Chr_length
 	# logging.info("Resolving the chromsome %s."%(Chr_name))
 	for read in samfile.fetch(Chr_name):
 		parse_read(read, Chr_name, 50, 20, 7, 2000)
 	samfile.close()
+	print("[INFO]: Parse reads used %0.2f seconds."%(time.time() - starttime))
 
-	show_temp_result(5, 50, 10, 1000)
+	show_temp_result(5, 50, 10, 1000, out_path)
+	print("[INFO]: Finished in %0.2f seconds."%(time.time() - starttime))
 
 if __name__ == '__main__':
-	main_ctrl(sys.argv[1])
+	main_ctrl(sys.argv[1], sys.argv[2])
