6959a5b6417e75a395e94f7747a51780f1e2831f
tjiang <tjiang@hit.edu.cn>
2018-09-20 16:31:42 +0800

    need to add shared memory

--- src/contrl.py
+++ src/contrl.py
@@ -17,16 +17,19 @@ def main_ctrl(sam_path, out_path):
 	for i in samfile.get_index_statistics():
 		process_list.append([i[0], i[3]])
 		# #chr #read
-	process_list = sorted(process_list, key = lambda x:-x[1])
-	Chr_name = process_list[0][0]
-	print("[INFO]: %d reads on %s."%(process_list[0][1], process_list[0][0]))
+	process_list = sorted(process_list, key = lambda x:x[1])
+
+	for i in process_list:
+		Chr_name = i[0]
+		local_starttime = time.time()
+		print("[INFO]: %d reads on %s."%(i[1], i[0]))
 	# Chr_length = samfile.get_reference_length(Chr_name)
 	# print Chr_name, Chr_length
 	# logging.info("Resolving the chromsome %s."%(Chr_name))
-	for read in samfile.fetch(Chr_name):
-		parse_read(read, Chr_name, 50, 20, 7, 2000)
+		for read in samfile.fetch(Chr_name):
+			parse_read(read, Chr_name, 50, 20, 4, 2000)
+		print("[INFO]: Parse reads used %0.2f seconds."%(time.time() - local_starttime))
 	samfile.close()
-	print("[INFO]: Parse reads used %0.2f seconds."%(time.time() - starttime))
 
 	show_temp_result(5, 50, 10, 1000, out_path)
 	print("[INFO]: Finished in %0.2f seconds."%(time.time() - starttime))
