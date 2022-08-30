a4f28fe9ae4da6001eb57b2eb0c6b4b0ca080c1c
JiangTao <tjiang@hit.edu.cn>
2022-08-30 13:01:49 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as f:
 
 setup(
     name = "cuteSV",
-    version = "1.0.13",
+    version = "2.0.0",
     description = "Long-read-based human genomic structural variation detection with cuteSV",
     author = "Jiang Tao",
     author_email = "tjiang@hit.edu.cn",
